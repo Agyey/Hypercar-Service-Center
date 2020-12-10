21f4a19a8337c607a8b68c412acc1362d2c174f4
Agyey Arya <agyey27@yahoo.co.in>
2020-12-10 20:07:00 +0530

    Service Menu

--- tickets/views.py
+++ tickets/views.py
@@ -1,7 +1,26 @@
 from django.views import View
 from django.http.response import HttpResponse
+from django.shortcuts import render
 
+services = [
+    {
+        'name': 'Change oil',
+        'path': 'change_oil'
+    },
+    {
+        'name': 'Inflate tires',
+        'path': 'inflate_tires'
+    },
+    {
+        'name': 'Get diagnostic test',
+        'path': 'diagnostic'
+    },
+]
 
 class WelcomeView(View):
     def get(self, request, *args, **kwargs):
         return HttpResponse('<h2>Welcome to the Hypercar Service!</h2>')
+
+class MenuView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'tickets\menu.html', context={'services': services})
